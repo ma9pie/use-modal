bb61629533ba07a9784a44458f9af66da62b6713
ma9pie <ma9pie.dev@gmail.com>
2024-03-08 17:00:19 +0900

    fix : rollup plugin typescript 추가

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
-import alias from '@rollup/plugin-alias';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import url from '@rollup/plugin-url';
 import svgr from '@svgr/rollup';
-import path from 'path';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 import pkg from './package.json';
@@ -17,10 +15,9 @@ const config = {
   plugins: [
     peerDepsExternal(),
     resolve({ extensions }),
-    typescript(),
-    // alias({
-    //   entries: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-    // }),
+    typescript({
+      exclude: ['**/*.stories.tsx', '**/*.test.ts', '**/*.test.tsx'],
+    }),
     commonjs({
       include: 'node_modules/**',
     }),
